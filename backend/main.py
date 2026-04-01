94754cf6b60a04597fa9114dd789a062a27e9560
Vupiad <12345vupham@gmail.com>
2026-04-01 21:46:04 +0700

    dependency hell, wasted time

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from api.v1.endpoints import sensors
 from dotenv import load_dotenv
+from database.postgres_db import engine, Base
 
 load_dotenv()
 
@@ -12,3 +13,13 @@ app.include_router(sensors.router, prefix="/api/v1/sensors", tags=["Sensors"])
 @app.get("/")
 def read_root():
     return {"status": "Online", "owner": "Vũ"}
+
+
+
+
+@app.on_event("startup")
+async def init_db():
+    async with engine.begin() as conn:
+        # This creates all tables defined in models.py if they don't exist
+        await conn.run_sync(Base.metadata.create_all)
+    print(" [DB] PostgreSQL Tables Created/Verified.")
